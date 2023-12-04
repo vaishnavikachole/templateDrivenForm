c004f61efaa60c48ab6c7a27825aab6a6a792a0e
Vaishnavi Kachole <vaishnavi.kachole@expleogroup.com>
2023-12-04 19:39:44 +0530

    template driven form in angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'templateDrivenForm';
+
+  showValue(data:any){
+    console.log(data)
+  }
+}
